83a660d023353187c5155d0daaa66cd48d8fb033
andtsouris <andtsouris@gmail.com>
2024-07-09 12:22:33 +0200

    Added maste bash script to launch snakemake with sbatch

---
+++ run_snakemake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=RNAseqPipeline
+#SBATCH --mail-type=END,FAIL
+#SBATCH --nodes=2
+#SBATCH --cores=16
+#SBATCH --output=log_run.%j.log
+#SBATCH --error=log_run.%j.log
+#SBATCH --open-mode=append
+
+
+/software/global/snakemake/v7.32.4/bin/snakemake \
+	--snakefile workflow/Snakefile \
+	--configfile config_cluster.yml \
+	--cores 16 \
+	--local-cores 4 
